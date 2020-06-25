1465266328643e513f732a85977f622bf8f0e30a
Dan <fujimud@yahoo.com>
2020-06-25 09:25:14 -0700

    Training useState

--- Training/hooks/src/App.js
+++ Training/hooks/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //const [count, setCount] = useState(4)
+  //const [state, setState] = useState({count:4, theme: 'blue'})  // object
+  //const count = state.count
+  //const theme = state.theme
+  const [count, setCount] = useState(4)
+  const [theme, setTheme] = useState('blue')
+
+  function decrementCount() {
+    //setCount(count - 1)  this works but is the incorrect way to do this
+    setCount(preCount => preCount - 1)
+    //setState(prevState => {
+    //  return {...prevState, count: prevState.count - 1}
+    //})
+  }
+
+  function incrementCount() {
+    setCount(preCount => preCount + 1)
+    setTheme('red')
+  }
+
+  return ( 
+    <>
+      <button onClick={decrementCount}>-</button> 
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
+    </>
+  )
 }
 
 export default App;
